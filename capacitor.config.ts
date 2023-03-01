344037116fc1f26b4fae39284b9d626bd130e281
phoebesu1025 <phoebesu85@yahoo.com.tw>
2023-03-01 22:24:32 +1300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '0301-ionic',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
